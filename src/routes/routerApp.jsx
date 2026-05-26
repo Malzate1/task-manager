e262758706fd03e685dd25114b9bb53795db7725
Mayra Alzate <nadja.minni@gmail.com>
2026-05-25 22:56:51 -0500

    "chore:localstorage/routerApp set"

---
+++ src/routes/routerApp.jsx
@@ -0,0 +1,15 @@
+import Login1 from "../pages/Login1";
+import Task1 from "../pages/Task1";
+
+
+export let routerApp = [
+  {
+    path: "/",
+    element: <Login1 />,
+  },
+{
+    path:"/task1",
+    element:<Task1 />
+},
+  
+];
